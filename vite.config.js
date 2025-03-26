6f71cb80a296de2dfaf07c276d455639f5e943a4
Ayesha Chaudhry <ayeshachaudhry@Ayeshas-Laptop.local>
2025-03-26 13:30:52 -0400

    Updates

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/AyeshaC123.github.io/',
+  base: '/AyeshaC123.github.io/',  // <== trailing slash is important
   plugins: [react()],
 })
